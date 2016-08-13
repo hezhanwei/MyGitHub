9ae9b5ab57af0209b70a8e1ddfac68c93d7f19c2
hezhanwei <18148010182@163.com>
2016-08-13 17:53:32 +0800

    add thread method
    z增加了多线程

--- hkonline/src/com/bluemobi/Main.java
+++ hkonline/src/com/bluemobi/Main.java
@@ -5,5 +5,13 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		String str = "helloworld petter";
+		
+		new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				System.out.println("你知道个毛");
+			}
+		}).start();
 	}
 }
