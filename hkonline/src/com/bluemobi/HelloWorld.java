4aa1256e98af4f3638b28afe9844458955d17544
hezhanwei <18148010182@163.com>
2016-08-14 12:02:49 +0800

    test method

--- hkonline/src/com/bluemobi/HelloWorld.java
+++ hkonline/src/com/bluemobi/HelloWorld.java
@@ -1,9 +1,16 @@
 package com.bluemobi;
 
+import org.junit.Test;
+
 public class HelloWorld {
 
 	//main
 	public static void main(String[] args) {
 		System.out.println("Hi I'm hello world");
+	}
+	
+	@Test
+	public void test(){
+		System.out.println("I'am test method");
 	}
 }
